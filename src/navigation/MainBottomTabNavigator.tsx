71dcae4fb7ab83ee85ba2b4aa04c6ee812764249
BlessingAdewale <adelekeblessingadewale@gmail.com>
2024-01-02 16:58:46 +0100

    Updated RootStack Function Component and also declared const RootStack using createNativeStackNavigator

---
+++ src/navigation/MainBottomTabNavigator.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const MainBottomTabNavigator = () => {
+  return (
+    <View>
+      <Text>MainBottomTabNavigator</Text>
+    </View>
+  )
+}
+
