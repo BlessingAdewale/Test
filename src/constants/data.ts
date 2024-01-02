67bb3f69f943d17e0d103c7f55874865611141a4
BlessingAdewale <adelekeblessingadewale@gmail.com>
2024-01-02 19:19:17 +0100

    Added all the dummyData

---
+++ src/constants/data.ts
@@ -0,0 +1,70 @@
+export const livestockData = [
+    {
+        id: '1',
+        name: 'Goat',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    },
+    {
+        id: '2',
+        name: 'Chicken',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    }
+]
+
+export const refrigeratedData = [
+    {
+        id: '1',
+        name: 'Grapes',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    },
+    {
+        id: '2',
+        name: 'Mangoes',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    }
+]
+
+export const dryProductsData = [
+    {
+        id: '1',
+        name: 'Yam',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    },
+    {
+        id: '2',
+        name: 'Beans',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    }
+]
+
+export const farmInputsData = [
+    {
+        id: '1',
+        name: 'Farm Boost',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    },
+    {
+        id: '2',
+        name: 'Agro Fertilizer',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    }
+]
+
+
+
