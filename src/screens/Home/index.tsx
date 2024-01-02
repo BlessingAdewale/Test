094e9b06f721dbeffb2d6c5f738439e044d954e6
BlessingAdewale <adelekeblessingadewale@gmail.com>
2024-01-02 16:51:03 +0100

    Added indexes to both home and product details

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>This is Home</Text>
+    </View>
+  )
+}
+
---
+++ src/screens/ProductDetails/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const ProductDetails = () => {
+  return (
+    <View>
+      <Text>This is ProductDetails</Text>
+    </View>
+  )
+}
+
