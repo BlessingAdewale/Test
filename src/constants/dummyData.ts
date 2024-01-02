7ee73813fc955ce2aae8331f4d21b9725b57ba5c
BlessingAdewale <adelekeblessingadewale@gmail.com>
2024-01-02 17:17:17 +0100

    started Dummy Data and created livestockData

---
+++ src/constants/dummyData.ts
@@ -0,0 +1,36 @@
+export const livestockData = [
+    {
+        id: '1',
+        name: 'Goat',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    },
+    {
+        id: '2',
+        name: 'Chicken',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    }
+]
+
+export const refrigeratedData = [
+    {
+        id: '1',
+        name: 'Goat',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    },
+    {
+        id: '2',
+        name: 'Chicken',
+        rated: '4.5',
+        numberOfPeopleThatRated: '92',
+        price: '10,000',
+    }
+]
+
+
+
