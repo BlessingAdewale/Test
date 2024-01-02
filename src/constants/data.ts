9691c2687ab9fc209b6ef09f395d79943301cb00
BlessingAdewale <adelekeblessingadewale@gmail.com>
2024-01-02 21:24:15 +0100

    Updated data with other information I had missed

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,70 +1,83 @@
 export const livestockData = [
-    {
-        id: '1',
-        name: 'Goat',
-        rated: '4.5',
-        numberOfPeopleThatRated: '92',
-        price: '10,000',
-    },
-    {
-        id: '2',
-        name: 'Chicken',
-        rated: '4.5',
-        numberOfPeopleThatRated: '92',
-        price: '10,000',
-    }
-]
+  {
+    id: '1',
+    name: 'Goat',
+    rated: '4.5',
+    numberOfPeopleThatRated: '92',
+    price: '10,000',
+    seller: 'Ajike Njoku',
+    note: 'Lorem ipsum dolor sit amet consectetur. Sit imperdiet tempus ut scelerisque in enim laoreet ut mi... Read More',
+  },
+  {
+    id: '2',
+    name: 'Chicken',
+    rated: '4.5',
+    numberOfPeopleThatRated: '92',
+    price: '10,000',
+    seller: 'Ajike Njoku',
+    note: 'Lorem ipsum dolor sit amet consectetur. Sit imperdiet tempus ut scelerisque in enim laoreet ut mi... Read More',
+  },
+];
 
 export const refrigeratedData = [
-    {
-        id: '1',
-        name: 'Grapes',
-        rated: '4.5',
-        numberOfPeopleThatRated: '92',
-        price: '10,000',
-    },
-    {
-        id: '2',
-        name: 'Mangoes',
-        rated: '4.5',
-        numberOfPeopleThatRated: '92',
-        price: '10,000',
-    }
-]
+  {
+    id: '1',
+    name: 'Grapes',
+    rated: '4.5',
+    numberOfPeopleThatRated: '92',
+    price: '10,000',
+    seller: 'Ajike Njoku',
+    note: 'Lorem ipsum dolor sit amet consectetur. Sit imperdiet tempus ut scelerisque in enim laoreet ut mi... Read More',
+  },
+  {
+    id: '2',
+    name: 'Mangoes',
+    rated: '4.5',
+    numberOfPeopleThatRated: '92',
+    price: '10,000',
+    seller: 'Ajike Njoku',
+    note: 'Lorem ipsum dolor sit amet consectetur. Sit imperdiet tempus ut scelerisque in enim laoreet ut mi... Read More',
+  },
+];
 
 export const dryProductsData = [
-    {
-        id: '1',
-        name: 'Yam',
-        rated: '4.5',
-        numberOfPeopleThatRated: '92',
-        price: '10,000',
-    },
-    {
-        id: '2',
-        name: 'Beans',
-        rated: '4.5',
-        numberOfPeopleThatRated: '92',
-        price: '10,000',
-    }
-]
+  {
+    id: '1',
+    name: 'Yam',
+    rated: '4.5',
+    numberOfPeopleThatRated: '92',
+    price: '10,000',
+    seller: 'Ajike Njoku',
+    note: 'Lorem ipsum dolor sit amet consectetur. Sit imperdiet tempus ut scelerisque in enim laoreet ut mi... Read More',
+  },
+  {
+    id: '2',
+    name: 'Beans',
+    rated: '4.5',
+    numberOfPeopleThatRated: '92',
+    price: '10,000',
+    seller: 'Ajike Njoku',
+    note: 'Lorem ipsum dolor sit amet consectetur. Sit imperdiet tempus ut scelerisque in enim laoreet ut mi... Read More',
+  },
+];
 
 export const farmInputsData = [
-    {
-        id: '1',
-        name: 'Farm Boost',
-        rated: '4.5',
-        numberOfPeopleThatRated: '92',
-        price: '10,000',
-    },
-    {
-        id: '2',
-        name: 'Agro Fertilizer',
-        rated: '4.5',
-        numberOfPeopleThatRated: '92',
-        price: '10,000',
-    }
-]
-
-
-
+  {
+    id: '1',
+    name: 'Farm Boost',
+    rated: '4.5',
+    numberOfPeopleThatRated: '92',
+    price: '10,000',
+    seller: 'Ajike Njoku',
+    note: 'Lorem ipsum dolor sit amet consectetur. Sit imperdiet tempus ut scelerisque in enim laoreet ut mi... Read More',
+  },
+  {
+    id: '2',
+    name: 'Agro Fertilizer',
+    rated: '4.5',
+    numberOfPeopleThatRated: '92',
+    price: '10,000',
+    seller: 'Ajike Njoku',
+    note: 'Lorem ipsum dolor sit amet consectetur. Sit imperdiet tempus ut scelerisque in enim laoreet ut mi... Read More',
+  },
+];
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -9,11 +9,16 @@ export const theme = {
     background: '#FFF',
     primary: '#006D33',
     secondary: '#F0F5EC',
+    tertiary: '#6D9E3F',
     whiteTextColor: '#FFF',
     blackTextCoor: '#090909',
     greetingColor: '#110',
+    redColor: '#DB371F',
     greyColor: '#BEBEBE',
-    grey2Color: '#8B8B8B'
-
+    grey2Color: '#8B8B8B',
+    grey3Color: '#686868',
+    grey4Color: '#585858',
+    arrowColor: '#1C1C1C',
+    bottomTabColor: 'rgba(0, 0, 0, 0.50)'
   },
 };
