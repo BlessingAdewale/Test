0e2fdbed2bb5a57d34631ebfe505ebfd6787a057
BlessingAdewale <adelekeblessingadewale@gmail.com>
2024-01-03 08:23:17 +0100

    Added all the images consts

---
+++ src/constants/images.ts
@@ -0,0 +1,19 @@
+const beans = require('../assets/images/dry/beans.png') as number;
+const yam = require('../assets/images/dry/yam.png') as number;
+const farmboost = require('../assets/images/farm/farmboost.png') as number;
+const fertilizer = require('../assets/images/farm/fertilizer.png') as number;
+const chicken = require('../assets/images/livestock/chicken.png') as number;
+const goat = require('../assets/images/livestock/goat.png') as number;
+const grapes = require('../assets/images/refrigerated/grapes.png') as number;
+const mangoes = require('../assets/images/refrigerated/mangoes.png') as number;
+
+export default {
+  beans,
+  yam,
+  farmboost,
+  fertilizer,
+  chicken,
+  goat,
+  grapes,
+  mangoes,
+};
